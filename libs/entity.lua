f9d7d893666175bb7caae27f4dce8b4df5ca7d4f
notyetawizard <notyetawizard@users.noreply.github.com>
2016-05-11 13:04:45 -0500

    Ready for client -> server movement!

--- libs/entity.lua
+++ libs/entity.lua
@@ -1,6 +1,11 @@
 entity = {}
 
 function entity:move(dir)
+    if dir == "up" then self.y = self.y - 1
+    elseif dir == "down" then self.y = self.y + 1
+    elseif dir == "left" then self.x = self.x - 1
+    elseif dir == "right" then self.x = self.x + 1
+    end
 end
 
 function entity.new(x, y)
